32cd9b230eeb8cd3ddfb8dea5db0b7f3e93f5a64
stijnstroeve <stijnstroeve@hotmail.com>
2019-12-16 00:34:26 +0100

    Added blueprint class. Added blueprint manager.

---
+++ Assets/Source/Blueprints/BlueprintBlock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Source.Blueprints
+{
+    class BlueprintBlock
+    {
+        protected string blockTypeID;
+        public string BlockTypeID
+        {
+            get => blockTypeID;
+        }
+
+        protected Vector2 position;
+        public Dictionary<string, float> Position
+        {
+            get => new Dictionary<string, float>
+            {
+                { "x", position.x },
+                { "y", position.y }
+            };
+        }
+
+        public BlueprintBlock(string blockTypeID, Vector2 position)
+        {
+            this.blockTypeID = blockTypeID;
+            this.position = position;
+        }
+    }
+}
