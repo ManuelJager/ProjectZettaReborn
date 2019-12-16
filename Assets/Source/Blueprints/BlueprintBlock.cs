5b0f4ec3e67659f997fdb4df299665d39fc1e4e8
stijnstroeve <stijnstroeve@hotmail.com>
2019-12-16 19:20:53 +0100

    Updated namespaces

--- Assets/Source/Blueprints/BlueprintBlock.cs
+++ Assets/Source/Blueprints/BlueprintBlock.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Assets.Source.Blueprints
+namespace Blueprints
 {
     class BlueprintBlock
     {
--- Assets/Source/Blueprints/BlueprintManager.cs
+++ Assets/Source/Blueprints/BlueprintManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine;
 
-namespace Assets.Source.Blueprints
+namespace Blueprints
 {
     class BlueprintManager : MonoBehaviour
     {
