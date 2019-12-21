601163a6bc1a8f07e0f2b6e16bad3ff2b642e9d6
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2019-12-21 17:41:49 +0100

    LazySingleton

---
+++ Assets/Source/Generics/LazySingleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class LazySingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static readonly Lazy<T> LazyInstance = new Lazy<T>(CreateSingleton);
+
+    public static T Instance => LazyInstance.Value;
+
+    private static T CreateSingleton()
+    {
+        var ownerObject = new GameObject($"{typeof(T).Name} (singleton)");
+        var instance = ownerObject.AddComponent<T>();
+        DontDestroyOnLoad(ownerObject);
+        return instance;
+    }
+
+    // call this from inherited class to initialize
+    public static void Echo()
+    {
+        Debug.Log($"{Instance.GetType().Name} echoe'd");
+    }
+}
