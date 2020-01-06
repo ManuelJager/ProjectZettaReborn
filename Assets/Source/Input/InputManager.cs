cd68cda940963275f582e3a7fe8725be7bc34dc7
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2020-01-06 23:07:28 +0100

    Pause menu

--- Assets/Source/Input/InputManager.cs
+++ Assets/Source/Input/InputManager.cs
@@ -9,23 +9,15 @@ namespace Zetta.InputWrapper
     public partial class InputManager : LazySingleton<InputManager>
     {
         public delegate void UpdateDelegate();
-
-        public static UpdateDelegate UpdateEvent;
-
         public delegate void ButtonActionClickDelegate();
-
-        public static event ButtonActionClickDelegate ClickShift;
-
-        public static event ButtonActionClickDelegate ClickEsc;
-
-        public static event ButtonActionClickDelegate ClickF10;
-
         public delegate void ButtonKeypressClickDelegate(char keyPressed);
-
-        public static event ButtonKeypressClickDelegate ClickKeypress;
-
         public delegate void InputAxisDelegate(Vector2 input);
 
+        public static event UpdateDelegate UpdateEvent;
+        public static event ButtonKeypressClickDelegate ClickKeypress;
+        public static event ButtonActionClickDelegate ClickShift;
+        public static event ButtonActionClickDelegate ClickEsc;
+        public static event ButtonActionClickDelegate ClickF10;
         public static event InputAxisDelegate InputAxis;
 
         private void OnGUI()
---
+++ Assets/Source/UI/UIWindows/UIWindowTabContent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zetta.UI.UIWindows
+{
+    public class UIWindowTabContent : MonoBehaviour
+    {
+        public GameObject tabHeader;
+        public RectTransform rectTransform;
+        public string tabName;
+
+        private void Start()
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+    }
+}
+
